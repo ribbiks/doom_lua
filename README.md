efd0fc4a52ed809a204c910dc49d2b7eb3093e31
ribbiks <ribbiks@gmail.com>
2021-05-22 14:09:05 -0400

    adding floodfill_things_random.lua

--- README.md
+++ README.md
@@ -20,6 +20,11 @@ A collection of scripts for Doom Builder X or the lua fork of Ultimate Doom Buil
 * Adds border linedefs to the interior of all selected linedefs.
 * Useful for adding vertical border/support textures to oddly-rotated geometry
 
+### floodfill_things_random.lua
+
+* Randomly scatters things in selected sectors.
+* Based on anotak's "sector_fill_things" script.
+
 ### join_identical_sectors.lua
 
 * Join (or merge) all selected sectors that have the same floor, ceiling, brightness, tag, effect, floortex, and ceiltex.
---
+++ floodfill_things_random.lua
@@ -0,0 +1,58 @@
+-- based off of sector_fill_things.lua by anotak
+
+function get_sector_bounding_box(sector)
+	local lines  = sector.GetLinedefs()
+	local center = sector.GetCenter()
+	local east   = center.x
+	local west   = center.x
+	local north  = center.y
+	local south  = center.y
+	for _,line in ipairs(lines) do
+		-- v1
+		west = math.min(west, line.start_vertex.position.x)
+		east = math.max(east, line.start_vertex.position.x)
+		south = math.min(south, line.start_vertex.position.y)
+		north = math.max(north, line.start_vertex.position.y)
+		-- v2
+		west = math.min(west, line.end_vertex.position.x)
+		east = math.max(east, line.end_vertex.position.x)
+		south = math.min(south, line.end_vertex.position.y)
+		north = math.max(north, line.end_vertex.position.y)
+	end
+	return east, west, north, south
+end
+
+--
+sectors = Map.GetSelectedSectors()
+if #sectors < 1 then
+	UI.LogLine("Must select at least 1 sector!")
+end
+
+UI.AddParameter("thing_id", "thing id", 3004)
+UI.AddParameter("density", "density (things/mu^2)", 0.002)
+UI.AddParameter("rng", "rng seed", 123)
+parameters = UI.AskForParameters()
+
+tid = tonumber(parameters.thing_id)
+dns = tonumber(parameters.density)
+
+math.randomseed(tonumber(parameters.rng))
+
+for i=1, #sectors do
+	e, w, n, s  = get_sector_bounding_box(sectors[i])
+	approx_area = (n-s)*(e-w)
+	things_to_place = math.ceil(approx_area*dns)
+	UI.LogLine("sector: " .. tostring(i) .. " things_to_place: " .. tostring(things_to_place))
+
+	for j=1, things_to_place do
+		tx = math.random(e,w)
+		ty = math.random(s,n)
+		local new_thing = Map.InsertThing(tx,ty)
+		new_thing.type  = tid
+		new_thing.SetAngleDoom(0)
+		thing_sector = Map.DetermineSector(new_thing.position)
+		if thing_sector == nil or thing_sector.GetIndex() ~= sectors[i].GetIndex() then
+			new_thing.Dispose()
+		end
+	end
+end
